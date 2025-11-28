e47a8fe0df2ec2c362df28099a3a921d1f3c0998
FIXED-TERM Doan Thi Khanh Linh (MS/EDS32-XC) <fixed-term.Linh.DoanThiKhanh@vn.bosch.com>
2025-11-28 16:31:11 +0700

    Update CD workflow

--- modules/version.tf
+++ modules/version.tf
@@ -35,11 +35,6 @@ provider "aws" {
   alias  = "bedrock"
   region = var.bedrock_region
 
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-
   # Disable SSL verification for proxy
   insecure = true
 }
